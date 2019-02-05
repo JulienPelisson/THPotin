4bd990cf53325cdc25944cdc51790c825c3e2243
JulienTHP <jpelisson@hotmail.fr>
2019-02-05 22:03:08 +0100

    ULTRACASSEBURNE

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # get '/user/:id/:user', to: 'index#profile', as: 'profiles'
-  # get '/user/:id', to: 'index#potin', as: 'potins'
-  # get '/contact', to: 'contact#home'
-  # get '/team', to: 'team#home' 
-  # 	erb :index, locals: {gossip: Gossip.all}
-  # end
-
-  resources :gossips do
-  	resources :comments
+  get '/user/:id/:user', to: 'index#profile', as: 'profiles'
+  get '/user/:id', to: 'index#potin', as: 'potins'
+  get '/contact', to: 'contact#home'
+  get '/team', to: 'team#home'
+  get '/', to: 'index#home' do 
+  	erb :index, locals: {gossip: Gossip.all}
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
