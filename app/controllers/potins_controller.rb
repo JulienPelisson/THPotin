b683a63a930dd583da8ced80547866d9243e2567
JulienTHP <jpelisson@hotmail.fr>
2019-02-06 10:30:24 +0100

    ULTRACASSEBURNE

--- app/controllers/potins_controller.rb
+++ app/controllers/potins_controller.rb
@@ -1,17 +1,22 @@
 class PotinsController < ApplicationController
  def index
-    # Méthode qui récupère tous les potins et les envoie à la view index (index.html.erb) pour affichage
+   def home 
+    @potin = Potin.all
+  end 
   end
 
   def show
-    # Méthode qui récupère le potin concerné et l'envoie à la view show (show.html.erb) pour affichage
+  @potin = Potin.all 
   end
 
   def new
+
     # Méthode qui crée un potin vide et l'envoie une view qui affiche le formulaire pour 'le remplir' (new.html.erb)
   end
 
   def create
+
+
     # Méthode qui créé un potin à partir du contenu du formulaire de new.html.erb, soumis par l'utilisateur
     # pour info, le contenu de ce formulaire sera accessible dans le hash params (ton meilleur pote)
     # Une fois la création faite, on redirige généralement vers la méthode show (pour afficher le potin créé)
